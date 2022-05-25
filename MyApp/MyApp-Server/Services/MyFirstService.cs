7117187b19a9eca789a044403ef2b0f8012b2afa
Takatoshi.lee <knig80005@gmail.com>
2022-05-25 11:14:22 +0800

    Upload server file.

---
+++ MyApp/MyApp-Server/Services/MyFirstService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MagicOnion;
+using MagicOnion.Server;
+using MyApp.Shared.Services;
+
+namespace MyApp.Services
+{
+    // Implements RPC service in the server project.
+    // The implementation class must inehrit `ServiceBase<IMyFirstService>` and `IMyFirstService`
+    public class MyFirstService : ServiceBase<IMyFirstService>, IMyFirstService
+    {
+        // `UnaryResult<T>` allows the method to be treated as `async` method.
+        public async UnaryResult<int> SumAsync(int x, int y)
+        {
+            Console.WriteLine($"Received:{x}, {y}");
+            return x + y;
+        }
+        public async UnaryResult<int> SendTest(MyClass myClass)
+        {
+            Console.WriteLine($"Received:{myClass.s} {myClass.f}");
+            return 0;
+        }
+    }
+}
